daa2fd990e8256586f1174736618c2f547cd1b9b
Ben Corcoran <ben.corky21@gmail.com>
2025-03-21 12:09:33 +0000

    added new implementations (yet to be populated) and altered the form to load them upon selection via the combo box

--- PriorityQueue/QueueManager.cs
+++ PriorityQueue/QueueManager.cs
@@ -26,13 +26,54 @@ namespace PriorityQueue
             if (CB_Implementation.SelectedIndex == 0)
             {
                 InitSortedArrayQueue();
+            } 
+            else if (CB_Implementation.SelectedIndex == 1)
+            { 
+                InitUnsortedArrayQueue();
             }
+            else if (CB_Implementation.SelectedIndex == 2)
+            {
+                InitSortedLinkedQueue();
+            }
+            else if (CB_Implementation.SelectedIndex == 3)
+            {
+                InitUnsortedLinkedQueue();
+            }
+            else if (CB_Implementation.SelectedIndex == 4)
+            {
+                InitHeapQueue();
+            }
+
         }
         
         private void InitSortedArrayQueue()
         {
             queue = new SortedArrayPriorityQueue<Person>(8);
             Lbl_Output.Text = "New sorted array priority queue created";
+        }
+
+        private void InitUnsortedArrayQueue()
+        {
+            queue = new UnsortedArrayPriorityQueue<Person>(8);
+            Lbl_Output.Text = "New unsorted array priority queue created";
+        }
+
+        private void InitSortedLinkedQueue()
+        {
+            queue = new SortedLinkedPriorityQueue<Person>(8);
+            Lbl_Output.Text = "New sorted linked list priority queue created";
+        }
+
+        private void InitUnsortedLinkedQueue()
+        {
+            queue = new UnsortedLinkedPriorityQueue<Person>(8);
+            Lbl_Output.Text = "New unsorted linked list priority queue created";
+        }
+
+        private void InitHeapQueue()
+        {
+            queue = new HeapPriorityQueue<Person>(8);
+            Lbl_Output.Text = "New heap priority queue created";
         }
 
         private void Btn_AddQueue_Click(object sender, System.EventArgs e)
---
+++ PriorityQueue/UnsortedLinkedPriorityQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    internal class UnsortedLinkedPriorityQueue<T> : PriorityQueue<T>
+    {
+        private readonly PriorityItem<T>[] storage;
+        private readonly int capacity;
+        private int tailIndex;
+
+        public UnsortedLinkedPriorityQueue(int size)
+        {
+            storage = new PriorityItem<T>[size];
+            capacity = size;
+            tailIndex = -1;
+        }
+
+        public void Add(T item, int priority)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Head()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
