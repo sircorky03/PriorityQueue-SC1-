8ea770e07ce9ae3495e744e288477b1dd35c4da8
Ben Corcoran <ben.corky21@gmail.com>
2025-03-21 12:51:29 +0000

    able to add elements to the unsorted array implementation but unable to remove without error.

--- PriorityQueue/UnsortedArrayPriorityQueue.cs
+++ PriorityQueue/UnsortedArrayPriorityQueue.cs
@@ -19,24 +19,85 @@ namespace PriorityQueue
             tailIndex = -1;
         }
 
-        public void Add(T item, int priority)
-        {
-            throw new NotImplementedException();
-        }
-
         public T Head()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException();
+            }
+
+            int highestPriorityIndex = 0;
+            for (int i = 1; i <= tailIndex; i++)
+            {
+                if (storage[i].Priority > storage[highestPriorityIndex].Priority)
+                {
+                    highestPriorityIndex = i;
+                }
+            }
+            return storage[highestPriorityIndex].Item;
         }
 
-        public bool IsEmpty()
+        public void Add(T item, int priority)
         {
-            throw new NotImplementedException();
+            tailIndex++;
+            if (tailIndex >= capacity)
+            {
+                tailIndex--;
+                throw new QueueOverflowException();
+            }
+
+            storage[tailIndex] = new PriorityItem<T>(item, priority);
         }
 
         public void Remove()
         {
-            throw new NotImplementedException();
+            //throw exception if empty
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException();
+            }
+
+            int highestPriorityIndex = 0;
+            for (int i = 1; i <= tailIndex; i++)
+            {
+                if (storage[i].Priority > storage[highestPriorityIndex].Priority)
+                {
+                    highestPriorityIndex = i;
+                }
+            }
+
+            //shift elements to remove the highest priority item
+            for (int i = highestPriorityIndex; i < tailIndex; i++)
+            {
+                storage[i] = storage[i + 1];
+            }
+
+            tailIndex++;
+        }
+
+        public override string ToString()
+        {
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException("No items to display");
+            }
+
+            string result = "[";
+            for (int i = 0; i <= tailIndex; i++)
+            {
+                if (i > 0)
+                {
+                    result += ", ";
+                }
+                result += storage[i];
+            }
+            result += "]";
+            return result;
+        }
+
+        public bool IsEmpty()
+        {
+            return tailIndex < 0;
         }
     }
 }
