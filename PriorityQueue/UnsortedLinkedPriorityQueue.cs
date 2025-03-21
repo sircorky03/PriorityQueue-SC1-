20bcc40ca0f9b96fa0741c9c710034ee5b991d36
Ben Corcoran <ben.corky21@gmail.com>
2025-03-21 15:26:04 +0000

    unsorted linked list implementation allows elements to be added but cannot be removed without unhandled exception error

--- PriorityQueue/UnsortedLinkedPriorityQueue.cs
+++ PriorityQueue/UnsortedLinkedPriorityQueue.cs
@@ -8,35 +8,137 @@ namespace PriorityQueue
 {
     internal class UnsortedLinkedPriorityQueue<T> : PriorityQueue<T>
     {
-        private readonly PriorityItem<T>[] storage;
+        private PriorityItem<T> head;
         private readonly int capacity;
-        private int tailIndex;
+        private int count;
 
         public UnsortedLinkedPriorityQueue(int size)
         {
-            storage = new PriorityItem<T>[size];
-            capacity = size;
-            tailIndex = -1;
-        }
+            this.capacity = size;
+            head = null;
+            count = 0;
+        } //end method
 
+        /*
+         * method for adding a new element to the queue
+         */
         public void Add(T item, int priority)
         {
-            throw new NotImplementedException();
-        }
+            if (count >= capacity)
+            {
+                throw new QueueOverflowException();
+            }
 
+            PriorityItem<T> newItem = new PriorityItem<T>(item, priority);
+            newItem.Next = head;
+            head = newItem;
+            count++;
+        } //end method
+
+        /*
+         * method returns the element at the head of the queue
+         */
         public T Head()
         {
-            throw new NotImplementedException();
-        }
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException();
+            }
 
+            PriorityItem<T> highestPriorityItem = head;
+            PriorityItem<T> current = head.Next;
+
+            while (current != null)
+            {
+                if (current.Priority > highestPriorityItem.Priority)
+                {
+                    highestPriorityItem = current;
+                }
+                current = current.Next;
+            }
+
+            return highestPriorityItem.Item;
+        } //end method
+
+        /*
+         * 
+         */
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
-        }
+            return count == 0;
+        } //end method
 
+        /*
+         * 
+         */
         public void Remove()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException();
+            }
+
+            if(head.Next != null)
+            {
+                head = null;
+                count--;
+                return;
+            }
+
+            PriorityItem<T> previous = null;
+            PriorityItem<T> current = head;
+            PriorityItem<T> highestPriorityItem = head;
+
+            while (current != null)
+            {
+                if (current.Priority > highestPriorityItem.Priority)
+                {
+                    highestPriorityItem = current;
+                }
+                current = current.Next;
+            }
+
+            current = head;
+            previous = null;
+
+            if (highestPriorityItem == head)
+            {
+                head = head.Next;
+                count--;
+                return;
+            }
+
+            while (current != highestPriorityItem)
+            {
+                previous = current;
+                previous.Next = current.Next;
+            }
+
+            previous.Next = current.Next;
+            count--;
+        } //end method
+
+        public override string ToString()
+        {
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException("No items to display");
+            }
+
+            string result = "[";
+            PriorityItem<T> current = head;
+            while (current != null)
+            {
+                result += current.ToString();
+                if (current.Next != null)
+                {
+                    result += ", ";
+                }
+                current = current.Next;
+            }
+            result += "]";
+            return result;
         }
+
     }
 }
